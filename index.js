aa38df9590c9ce12373db4fff11c93b90a6903da
pbaki <pbaki999@gmail.com>
2023-03-25 14:02:35 +0100

    Simple Drop down menu added

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("asd");
+const dropdownToggle = document.querySelector(".dropdown-toggle");
+const dropdownList = document.querySelector(".dropdown-list").children;
+console.log(dropdownList);
+function toggle() {
+  for (item of dropdownList) {
+    console.log(item);
+    if (item.style.display === "block") {
+      item.style.display = "none";
+    } else item.style.display = "block";
+  }
+}
+
+dropdownToggle.addEventListener("click", toggle);
