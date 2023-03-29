a374ac9e6d06d4ecf54f0d90d2869e82791f7dd7
pbaki <pbaki999@gmail.com>
2023-03-29 18:42:52 +0200

    Added mouseover and mouseout to more class elements

--- index.js
+++ index.js
@@ -14,11 +14,9 @@ dropdownToggle.addEventListener("click", toggle);
 //code 2 - Mobile menu js
 const dropdownToggle2 = document.querySelector(".more");
 const dropdownList2 = document.querySelector(".dropdown-list2").children;
-console.log(dropdownToggle2);
 
 function toggle2() {
   for (item of dropdownList2) {
-    console.log(item);
     if (item.style.display === "block") {
       item.style.display = "none";
     } else item.style.display = "block";
@@ -26,3 +24,15 @@ function toggle2() {
 }
 
 dropdownToggle2.addEventListener("click", toggle2);
+
+dropdownToggle2.addEventListener("mouseover", () => {
+  dropdownToggle2.firstElementChild.style.filter =
+    "invert(77%) sepia(0%) saturate(309%) hue-rotate(135deg) brightness(85%) contrast(95%)";
+  dropdownToggle2.lastElementChild.style.color = "#A5A5A5";
+});
+
+dropdownToggle2.addEventListener("mouseout", () => {
+  dropdownToggle2.firstElementChild.style.filter =
+    "invert(98%) sepia(3%) saturate(0%) hue-rotate(83deg) brightness(115%) contrast(92%)";
+  dropdownToggle2.lastElementChild.style.color = "rgb(245, 245, 245)";
+});
