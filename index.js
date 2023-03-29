83729e58741fa750b70d97281421e6a3333ccc3a
pbaki <pbaki999@gmail.com>
2023-03-29 17:54:02 +0200

    Finished Mobile menu template

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+//code 1 - Dropdown menu js
 const dropdownToggle = document.querySelector(".dropdown-toggle");
 const dropdownList = document.querySelector(".dropdown-list").children;
-console.log(dropdownList);
 function toggle() {
   for (item of dropdownList) {
-    console.log(item);
     if (item.style.display === "block") {
       item.style.display = "none";
     } else item.style.display = "block";
@@ -11,3 +10,19 @@ function toggle() {
 }
 
 dropdownToggle.addEventListener("click", toggle);
+
+//code 2 - Mobile menu js
+const dropdownToggle2 = document.querySelector(".more");
+const dropdownList2 = document.querySelector(".dropdown-list2").children;
+console.log(dropdownToggle2);
+
+function toggle2() {
+  for (item of dropdownList2) {
+    console.log(item);
+    if (item.style.display === "block") {
+      item.style.display = "none";
+    } else item.style.display = "block";
+  }
+}
+
+dropdownToggle2.addEventListener("click", toggle2);
